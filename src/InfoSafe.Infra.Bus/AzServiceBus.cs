30566fd78e5bc7b32b2e331814e25fa2b421c5d3
Joe Ipe <Joe.Ipe@aemo.com.au>
2023-10-18 11:13:04 +1100

    clean up

--- src/InfoSafe.Infra.Bus/AzServiceBus.cs
+++ src/InfoSafe.Infra.Bus/AzServiceBus.cs
@@ -1,10 +1,5 @@
 ﻿using Azure.Messaging.ServiceBus;
 using InfoSafe.Infra.Bus.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InfoSafe.Infra.Bus
 {
--- src/InfoSafe.Infra.Bus/ExtraForLearning/RmqFanOutServiceBus.cs
+++ src/InfoSafe.Infra.Bus/ExtraForLearning/RmqFanOutServiceBus.cs
@@ -1,10 +1,6 @@
 ﻿using InfoSafe.Infra.Bus.Interfaces;
 using RabbitMQ.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace InfoSafe.Infra.Bus.ExtraForLearning
 {
